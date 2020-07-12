2d8924d2c6c423c0f1dbea56f01165a8aa5561d5
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-11 20:57:56 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,14 @@
-//your code here
+import React, { Component } from 'react';
+
+export default class Comment extends Component {
+  render() {
+    return (
+      <div className="comment">
+        {this.props.commentText}
+      </div>
+    )
+  }
+}
+
+
+// It should expect a single prop (the text of a comment), which can be used in the component via: this.props.commentText. This prop is passed in src/BlogPost.js
